27942f1d6018f07d0376db2abf38636c7c10be21
Maxime GELLE <maxime.gelle@gmail.com>
2021-10-28 20:23:34 +0200

    Add play card

--- src/tcg/core.clj
+++ src/tcg/core.clj
@@ -11,28 +11,32 @@
   (class {}))
 
 (defn startGame [state cmd] (let [player1Deck (:player1Deck cmd)
-      player2Deck (:player2Deck cmd)
-      [player1Card1 player1Card2 player1Card3, player1Card4] player1Deck
-      [player2Card1 player2Card2 player2Card3, player2Card4] player2Deck]
-  [{:evt :GameStarted
-    :player1Deck player1Deck
-    :player2Deck player2Deck}
-   {:evt :PlayerPickedACard :player :Player1 :cardPicked player1Card1}
-   {:evt :PlayerPickedACard :player :Player1 :cardPicked player1Card2}
-   {:evt :PlayerPickedACard :player :Player1 :cardPicked player1Card3}
-   {:evt :PlayerPickedACard :player :Player2 :cardPicked player2Card1}
-   {:evt :PlayerPickedACard :player :Player2 :cardPicked player2Card2}
-   {:evt :PlayerPickedACard :player :Player2 :cardPicked player2Card3}
-   {:evt :PlayerPickedACard :player :Player2 :cardPicked player2Card4}
-   {:evt :PlayerBecameActive :player :Player1}
-   {:evt :ReceivedManaSlot :player :Player1}
-   {:evt :ManaSlotsFilled :player :Player1}
-   {:evt :PlayerPickedACard :player :Player1 :cardPicked player1Card4}]))
+                                  player2Deck (:player2Deck cmd)
+                                  [player1Card1 player1Card2 player1Card3, player1Card4] player1Deck
+                                  [player2Card1 player2Card2 player2Card3, player2Card4] player2Deck]
+                              [{:evt :GameStarted
+                                :player1Deck player1Deck
+                                :player2Deck player2Deck}
+                               {:evt :PlayerPickedACard :player :Player1 :cardPicked player1Card1}
+                               {:evt :PlayerPickedACard :player :Player1 :cardPicked player1Card2}
+                               {:evt :PlayerPickedACard :player :Player1 :cardPicked player1Card3}
+                               {:evt :PlayerPickedACard :player :Player2 :cardPicked player2Card1}
+                               {:evt :PlayerPickedACard :player :Player2 :cardPicked player2Card2}
+                               {:evt :PlayerPickedACard :player :Player2 :cardPicked player2Card3}
+                               {:evt :PlayerPickedACard :player :Player2 :cardPicked player2Card4}
+                               {:evt :PlayerBecameActive :player :Player1}
+                               {:evt :ReceivedManaSlot :player :Player1}
+                               {:evt :ManaSlotsFilled :player :Player1}
+                               {:evt :PlayerPickedACard :player :Player1 :cardPicked player1Card4}]))
+
+(defn playCard [state cmd] [{:evt :CardPlayed :player :Player1 :card 1}
+                            {:evt :HealthLost :player :Player2}])
 
 (defn decide
   [state cmd]
   (cond
-    ((:cmd cmd) == :StartGame) (startGame state cmd)
+    (= (:cmd cmd) :StartGame) (startGame state cmd)
+    (= (:cmd cmd) :PlayCard) (playCard state cmd)
     :else []))
 
 (defn receive
--- test/tcg/core_test.clj
+++ test/tcg/core_test.clj
@@ -25,4 +25,16 @@
           {:evt :PlayerBecameActive :player :Player1}
           {:evt :ReceivedManaSlot :player :Player1}
           {:evt :ManaSlotsFilled :player :Player1}
-          {:evt :PlayerPickedACard :player :Player1 :cardPicked 3}]))))
+          {:evt :PlayerPickedACard :player :Player1 :cardPicked 3}])))
+
+  (testing "Play a card"
+    (is (=
+         (receive [{:evt :GameStarted
+                    :player1Deck [0 1 2 3 4 5]
+                    :player2Deck [5 4 3 2 1 0]}
+                   {:evt :PlayerPickedACard :player :Player1 :cardPicked 1}
+                   {:evt :PlayerBecameActive :player :Player1}
+                   {:evt :ReceivedManaSlot :player :Player1}
+                   {:evt :ManaSlotsFilled :player :Player1}] {:cmd :PlayCard :card 1})
+         [{:evt :CardPlayed :player :Player1 :card 1}
+          {:evt :HealthLost :player :Player2}]))))
