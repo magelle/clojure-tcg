7db19d9be1061ef08a73d7e11fd9b1807ac6deb6
Maxime GELLE <maxime.gelle@gmail.com>
2021-10-28 19:30:34 +0200

    PLayers pick cards from the deck

--- src/tcg/core.clj
+++ src/tcg/core.clj
@@ -12,16 +12,22 @@
 
 (defn decide
   [state cmd]
+  (let [
+        player1Deck (:player1Deck cmd)
+        player2Deck (:player2Deck cmd)
+        [player1Card1 player1Card2 player1Card3] player1Deck
+        [player2Card1 player2Card2 player2Card3, player2Card4] player2Deck
+  ]
   [{:evt "GameStarted"
-    :player1Deck (:player1Deck cmd)
-    :player2Deck (:player2Deck cmd)}
-   {:evt "PlayerPickedACard" :player "Player 1" :cardPicked 0}
-   {:evt "PlayerPickedACard" :player "Player 1" :cardPicked 1}
-   {:evt "PlayerPickedACard" :player "Player 1" :cardPicked 2}
-   {:evt "PlayerPickedACard" :player "Player 2" :cardPicked 5}
-   {:evt "PlayerPickedACard" :player "Player 2" :cardPicked 4}
-   {:evt "PlayerPickedACard" :player "Player 2" :cardPicked 3}
-   {:evt "PlayerPickedACard" :player "Player 2" :cardPicked 2}])
+    :player1Deck player1Deck
+    :player2Deck player2Deck}
+   {:evt "PlayerPickedACard" :player "Player 1" :cardPicked player1Card1}
+   {:evt "PlayerPickedACard" :player "Player 1" :cardPicked player1Card2}
+   {:evt "PlayerPickedACard" :player "Player 1" :cardPicked player1Card3}
+   {:evt "PlayerPickedACard" :player "Player 2" :cardPicked player2Card1}
+   {:evt "PlayerPickedACard" :player "Player 2" :cardPicked player2Card2}
+   {:evt "PlayerPickedACard" :player "Player 2" :cardPicked player2Card3}
+   {:evt "PlayerPickedACard" :player "Player 2" :cardPicked player2Card4}]))
 
 (defn receive
   [events command]
