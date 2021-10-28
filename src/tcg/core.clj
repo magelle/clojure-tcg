74487f1f24b97d367a13c6fa69ec1ceb6af67cf8
Maxime GELLE <maxime.gelle@gmail.com>
2021-10-28 12:05:51 +0200

    Define base functions

--- src/tcg/core.clj
+++ src/tcg/core.clj
@@ -5,3 +5,15 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(defn apply
+  [events]
+  (class {}))
+
+(defn decide
+  [state command]
+  ["GameStarted"])
+
+(defn receive
+  [events command]
+  (decide (apply events) command))
--- test/tcg/core_test.clj
+++ test/tcg/core_test.clj
@@ -7,7 +7,8 @@
   (testing "test a test !"
     (is (= 1 1)))
 
+(testing "apply"
+  (is (apply []) {}))
+
   (testing "begin a game"
-    (let [a 1
-          b 1]
-      (is (= a b)))))
+      (is (= (receive [] (class {:cmd "StartGame" :g 1})) ["GameStarted"]))))
