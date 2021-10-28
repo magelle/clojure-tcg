4b506e3db1c78b03714b5c2ef9cb64e9f9b323a1
Maxime GELLE <maxime.gelle@gmail.com>
2021-10-28 18:46:46 +0200

    Specify card in pickedCard

--- src/tcg/core.clj
+++ src/tcg/core.clj
@@ -15,12 +15,12 @@
   [{:evt "GameStarted"
     :player1Deck (:player1Deck cmd)
     :player2Deck (:player2Deck cmd)}
-   {:evt "PlayerPickedACard" :player "Player 1"}
-   {:evt "PlayerPickedACard" :player "Player 1"}
-   {:evt "PlayerPickedACard" :player "Player 1"}
-   {:evt "PlayerPickedACard" :player "Player 2"}
-   {:evt "PlayerPickedACard" :player "Player 2"}
-   {:evt "PlayerPickedACard" :player "Player 2"}])
+   {:evt "PlayerPickedACard" :player "Player 1" :cardPicked 0}
+   {:evt "PlayerPickedACard" :player "Player 1" :cardPicked 1}
+   {:evt "PlayerPickedACard" :player "Player 1" :cardPicked 2}
+   {:evt "PlayerPickedACard" :player "Player 2" :cardPicked 5}
+   {:evt "PlayerPickedACard" :player "Player 2" :cardPicked 4}
+   {:evt "PlayerPickedACard" :player "Player 2" :cardPicked 3}])
 
 (defn receive
   [events command]
--- test/tcg/core_test.clj
+++ test/tcg/core_test.clj
@@ -10,14 +10,14 @@
   (testing "begin a game"
     (is (=
          (receive [] {:cmd "StartGame"
-                      :player1Deck [0, 1, 2, 3, 4, 5]
-                      :player2Deck [0, 1, 2, 3, 4, 5]})
+                      :player1Deck [0 1 2 3 4 5]
+                      :player2Deck [5 4 3 2 1 0]})
          [{:evt "GameStarted"
-           :player1Deck [0, 1, 2, 3, 4, 5]
-           :player2Deck [0, 1, 2, 3, 4, 5]}
-          {:evt "PlayerPickedACard" :player "Player 1"}
-          {:evt "PlayerPickedACard" :player "Player 1"}
-          {:evt "PlayerPickedACard" :player "Player 1"}
-          {:evt "PlayerPickedACard" :player "Player 2"}
-          {:evt "PlayerPickedACard" :player "Player 2"}
-          {:evt "PlayerPickedACard" :player "Player 2"}]))))
+           :player1Deck [0 1 2 3 4 5]
+           :player2Deck [5 4 3 2 1 0]}
+          {:evt "PlayerPickedACard" :player "Player 1" :cardPicked 0}
+          {:evt "PlayerPickedACard" :player "Player 1" :cardPicked 1}
+          {:evt "PlayerPickedACard" :player "Player 1" :cardPicked 2}
+          {:evt "PlayerPickedACard" :player "Player 2" :cardPicked 5}
+          {:evt "PlayerPickedACard" :player "Player 2" :cardPicked 4}
+          {:evt "PlayerPickedACard" :player "Player 2" :cardPicked 3}]))))
