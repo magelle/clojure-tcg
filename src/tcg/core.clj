e8bb5cd397eb1323798f3b52c2634fc751843445
Maxime GELLE <maxime.gelle@gmail.com>
2021-10-28 20:07:29 +0200

    Extract start game in a fonction

--- src/tcg/core.clj
+++ src/tcg/core.clj
@@ -10,27 +10,29 @@
   [events]
   (class {}))
 
+(defn startGame [state cmd] (let [player1Deck (:player1Deck cmd)
+      player2Deck (:player2Deck cmd)
+      [player1Card1 player1Card2 player1Card3, player1Card4] player1Deck
+      [player2Card1 player2Card2 player2Card3, player2Card4] player2Deck]
+  [{:evt :GameStarted
+    :player1Deck player1Deck
+    :player2Deck player2Deck}
+   {:evt :PlayerPickedACard :player :Player1 :cardPicked player1Card1}
+   {:evt :PlayerPickedACard :player :Player1 :cardPicked player1Card2}
+   {:evt :PlayerPickedACard :player :Player1 :cardPicked player1Card3}
+   {:evt :PlayerPickedACard :player :Player2 :cardPicked player2Card1}
+   {:evt :PlayerPickedACard :player :Player2 :cardPicked player2Card2}
+   {:evt :PlayerPickedACard :player :Player2 :cardPicked player2Card3}
+   {:evt :PlayerPickedACard :player :Player2 :cardPicked player2Card4}
+   {:evt :PlayerBecameActive :player :Player1}
+   {:evt :ReceivedManaSlot :player :Player1}
+   {:evt :ManaSlotsFilled :player :Player1}
+   {:evt :PlayerPickedACard :player :Player1 :cardPicked player1Card4}]))
+
 (defn decide
   [state cmd]
   (cond
-    ((:cmd cmd) == :StartGame) (let [player1Deck (:player1Deck cmd)
-                                     player2Deck (:player2Deck cmd)
-                                     [player1Card1 player1Card2 player1Card3, player1Card4] player1Deck
-                                     [player2Card1 player2Card2 player2Card3, player2Card4] player2Deck]
-                                 [{:evt :GameStarted
-                                   :player1Deck player1Deck
-                                   :player2Deck player2Deck}
-                                  {:evt :PlayerPickedACard :player :Player1 :cardPicked player1Card1}
-                                  {:evt :PlayerPickedACard :player :Player1 :cardPicked player1Card2}
-                                  {:evt :PlayerPickedACard :player :Player1 :cardPicked player1Card3}
-                                  {:evt :PlayerPickedACard :player :Player2 :cardPicked player2Card1}
-                                  {:evt :PlayerPickedACard :player :Player2 :cardPicked player2Card2}
-                                  {:evt :PlayerPickedACard :player :Player2 :cardPicked player2Card3}
-                                  {:evt :PlayerPickedACard :player :Player2 :cardPicked player2Card4}
-                                  {:evt :PlayerBecameActive :player :Player1}
-                                  {:evt :ReceivedManaSlot :player :Player1}
-                                  {:evt :ManaSlotsFilled :player :Player1}
-                                  {:evt :PlayerPickedACard :player :Player1 :cardPicked player1Card4}])
+    ((:cmd cmd) == :StartGame) (startGame state cmd)
     :else []))
 
 (defn receive
