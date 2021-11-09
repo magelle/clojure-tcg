11139de97d3e0c67c9ce185bbd9ae91e0659a338
Maxime GELLE <maxime.gelle@gmail.com>
2021-11-09 18:23:54 +0100

    Player 2 can win a game too

--- test/tcg/core_test.clj
+++ test/tcg/core_test.clj
@@ -61,7 +61,7 @@
           {:evt :ManaSlotsFilled :player :Player2}
           {:evt :PlayerPickedACard :player :Player2 :cardPicked 5}])))
 
-  (testing "Can win a game when player deals with 30 damages"
+  (testing "Player1 can win a game when player deals with 30 damages"
     (is (=
          (receive (concat [{:evt :GameStarted
                             :player1Deck [0 1 2 3 4 5]
@@ -72,4 +72,18 @@
                            {:evt :ManaSlotsFilled :player :Player1}] (repeat 29 {:evt :HealthLost :player :Player2})) {:cmd :PlayCard :card 1})
          [{:evt :CardPlayed :player :Player1 :card 1}
           {:evt :HealthLost :player :Player2}
-          {:evt :WinGame :player :Player1}]))))
+          {:evt :WinGame :player :Player1}])))
+
+  (testing "Player2 can win a game when player deals with 30 damages"
+    (is (=
+         (receive (concat [{:evt :GameStarted
+                            :player1Deck [0 1 2 3 4 5]
+                            :player2Deck [5 4 1 2 3 0]}
+                           {:evt :PlayerEndedTurn :player :Player1}
+                           {:evt :PlayerPickedACard :player :Player2 :cardPicked 1}
+                           {:evt :PlayerBecameActive :player :Player2}
+                           {:evt :ReceivedManaSlot :player :Player2}
+                           {:evt :ManaSlotsFilled :player :Player2}] (repeat 29 {:evt :HealthLost :player :Player1})) {:cmd :PlayCard :card 1})
+         [{:evt :CardPlayed :player :Player2 :card 1}
+          {:evt :HealthLost :player :Player1}
+          {:evt :WinGame :player :Player2}]))))
